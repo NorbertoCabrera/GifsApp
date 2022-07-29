36181220d0e8b8808111c809585a5137da74c14f
Norberto <norberto.cabrera.a@gmail.com>
2022-07-29 19:40:19 -0300

    peticiones http y renderizado de gifs

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,22 +6,50 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
 
-  constructor() { }
+  
   private _historial:string[]=[]
+  private apiKey:string='oJP1NZ3xAErOEodh9BXCR7HAfkeaOSzr'
+  private url:string='api.giphy.com/v1/gifs/search?api_key=oJP1NZ3xAErOEodh9BXCR7HAfkeaOSzr&q=messi&limit=10'
+
+  public resultados : any[]=[];
+
 
   get historial(){
     return [...this._historial]
   }
 
+  
+  
+  constructor(private http:HttpClient){} // uso de httpClient en constructor
+
+
+
+
+  
+  //metodo para buscar Gifs-------------
   buscarGifs(query:string){
 
     query=query.trim().toLowerCase();
+
     if(!this._historial.includes(query)){
       this._historial.unshift(query);
       this._historial=this._historial.splice(0,10)
     } 
 
-    console.log(this._historial)
+    //console.log(this._historial)
+
+
+
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=oJP1NZ3xAErOEodh9BXCR7HAfkeaOSzr&q=${query}&limit=10`)
+        .subscribe((resp:any)=>{
+          console.log(resp.data)
+          this.resultados=resp.data;
+        })
   }
+
+   
+
+
+
 
 }
